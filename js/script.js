8cf4471fba88954caea1b72de62e78fce902522e
Daniele <daniele.bedeschi94@gmail.com>
2020-12-18 15:48:21 +0100

    Commit with axis

---
+++ js/script.js
@@ -0,0 +1,22 @@
+var app = new Vue (
+  {
+    el:'.root',
+    data: {
+      films:[],
+      address:"https://image.tmdb.org/t/p/w220_and_h330_face/"
+    },
+    mounted: function() {
+      axios
+      .get('https://api.themoviedb.org/3/search/movie', {
+        params: {
+          api_key: '1e26c6a15aa1b1d7e6be784af83e54ac',
+          query: 'harry'
+        }
+      })
+      .then((result) => {
+        this.films = result.data.results;
+        console.log(this.films[0].id);
+      });
+    }
+  }
+);
