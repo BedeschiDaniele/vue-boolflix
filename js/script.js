eda42118fdedcb3042fccfab434e0a92e55f74fd
Daniele <daniele.bedeschi94@gmail.com>
2020-12-21 17:23:03 +0100

    Commit serie non funziona

--- js/script.js
+++ js/script.js
@@ -3,7 +3,9 @@ var app = new Vue (
     el:'.root',
     data: {
       films:[],
+      series:[],
       arraylength:0,
+      arraylength1:0,
       votes:[],
       filmTitle:"",
       address:"https://image.tmdb.org/t/p/w220_and_h330_face/"
@@ -24,6 +26,20 @@ var app = new Vue (
         for (var i = 0; i < this.arraylength; i++) {
           this.votes.push((Math.ceil(this.films[i].vote_average/2)));
         }
+      });
+    },
+      searchseries: function() {
+      axios
+      .get('https://api.themoviedb.org/3/search/tv', {
+        params: {
+          api_key: '1e26c6a15aa1b1d7e6be784af83e54ac',
+          query: this.filmTitle,
+          language:'it-IT'
+        }
+      })
+      .then((result) => {
+        this.series = result.data.results;
+        console.log(this.series);
       });
     },
     setflag: function (film) {
